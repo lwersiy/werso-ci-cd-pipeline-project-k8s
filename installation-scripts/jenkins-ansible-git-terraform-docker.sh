6c3f07303a017870065410ac90e96b5912c6a964
loui wersiy <petitwerso2003@gmail.com>
2024-02-05 00:26:14 -0500

    updted jenkinsfile with docker install script

--- installation-scripts/jenkins-ansible-git-terraform-docker.sh
+++ installation-scripts/jenkins-ansible-git-terraform-docker.sh
@@ -43,6 +43,7 @@ sudo yum -y install terraform
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
+sudo service docker enable
 sudo usermod -aG docker jenkins
 sudo systemctl restart docker
 
